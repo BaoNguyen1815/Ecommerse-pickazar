a6533b118d10f452fb0c73211b8e5d84cf4bf0b7
vutienquoc123 <50628209+vutienquoc123@users.noreply.github.com>
2020-11-27 13:37:46 +0700

    product,user

---
+++ packages/api/shop/models/products.ts
@@ -0,0 +1,43 @@
+module.exports = function (sequelize: any, DataTypes: any) {
+
+    const products = sequelize.define("products", {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+        },
+        title: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          field: "name",
+        },
+        price: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+          field: "unit_price",
+        },
+        slug:{
+            type: DataTypes.STRING,
+            allowNull:true,
+        },
+        unit:{
+            type: DataTypes.STRING,
+            allowNull:true,
+        },
+        description:{
+            type: DataTypes.STRING,
+            allowNull:true,
+            field:"meta_description"
+        },
+        image:{
+          type: DataTypes.STRING,
+            allowNull:true,
+            field:"thumbnail_img"
+        }
+      }, {
+        tableName: "products",
+        freezeTableName: true,
+        timestamps: false,
+      })
+      return products;
+    };
